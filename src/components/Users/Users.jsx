a7df5c4844d2f2298cc416d5aa16caf00d60f33c
vahagnhov <vahagn505315v@gmail.com>
2022-10-22 01:13:03 +0400

    Life cycle methods, componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,7 +6,8 @@ import userNoPhoto from '../../assets/images/user.png';
 class Users extends React.Component {
     constructor(props) {
         super(props);
-
+    }
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items);
         });
